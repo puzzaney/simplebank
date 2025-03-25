90687cdc4243a6de61ed7a525b3c3d2479035153
PuzZan Karki <mail.puzzan@gmail.com>
2025-03-25 18:35:34 +0000

    initialize mock test

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // NOTE: Server serves http request to our banking service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // NOTE: NewServer Creates a HTTP server
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
