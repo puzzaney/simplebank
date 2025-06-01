3edc164fcbb22ecc496c1b8fa415c2de187dae2a
PuzZan Karki <mail.puzzan@gmail.com>
2025-06-01 11:37:16 +0000

    Add ok case for createAccountAPI

--- api/account_test.go
+++ api/account_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	mockdb "github.com/puzzaney/simplebank/db/mock"
 	db "github.com/puzzaney/simplebank/db/sqlc"
 	"github.com/puzzaney/simplebank/util"
@@ -82,6 +83,60 @@ func TestGetAccountAPI(t *testing.T) {
 
 			tc.checkResponse(t, recorder)
 		})
+	}
+}
+
+func TestCreateAccountAPI(t *testing.T) {
+	account := randomAccount()
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{{
+		name: "OK",
+		body: gin.H{
+			"owner":    account.Owner,
+			"currency": account.Currency,
+		},
+		buildStubs: func(store *mockdb.MockStore) {
+			arg := db.CreateAccountParams{
+				Owner:    account.Owner,
+				Currency: account.Currency,
+				Balance:  0,
+			}
+			store.EXPECT().CreateAccount(gomock.Any(), gomock.Eq(arg)).Times(1).Return(account, nil)
+		},
+		checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+			require.Equal(t, http.StatusOK, recorder.Code)
+			requireBodyMatchAccount(t, recorder.Body, account)
+		},
+	},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/accounts"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+		})
+
 	}
 }
 
