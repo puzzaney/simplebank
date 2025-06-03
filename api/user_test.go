8e39a44a4bf966ad3dd9a06252e76a4bae8c3da4
PuzZan Karki <mail.puzzan@gmail.com>
2025-06-03 02:41:23 +0000

    Add custom matcher for user_test

--- api/user_test.go
+++ api/user_test.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -18,6 +20,33 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
+type eqCreateUserParamsMatcher struct {
+	arg      db.CreateUserParams
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	err := util.CheckPassword(e.password, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+
+	e.arg.HashedPassword = arg.HashedPassword
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg, password}
+}
 func TestCreateUserAPI(t *testing.T) {
 	user, password := randomUser(t)
 
@@ -35,7 +64,12 @@ func TestCreateUserAPI(t *testing.T) {
 			"email":     user.Email,
 		},
 		buildStubs: func(store *mockdb.MockStore) {
-			store.EXPECT().CreateUser(gomock.Any(), gomock.Any()).Times(1).Return(user, nil)
+			arg := db.CreateUserParams{
+				Username: user.Username,
+				FullName: user.FullName,
+				Email:    user.Email,
+			}
+			store.EXPECT().CreateUser(gomock.Any(), EqCreateUserParams(arg, password)).Times(1).Return(user, nil)
 		},
 		checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
 			require.Equal(t, http.StatusOK, recorder.Code)
