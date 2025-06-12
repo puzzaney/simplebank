4b31fd2403eb7485daa20fa6a0c0a59c69d6c700
PuzZan Karki <mail.puzzan@gmail.com>
2025-06-12 06:05:38 +0000

    feat: use exact names used in proto files for response

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/puzzaney/simplebank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -64,7 +65,17 @@ func runGatewayServer(config util.Config, store db.Store) {
 		log.Fatal("cannot create gRPC server: %w", err)
 	}
 
-	grpcMux := runtime.NewServeMux()
+	//use the exact case used in the proto files i.e. camelcase
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
