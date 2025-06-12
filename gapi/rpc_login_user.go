b1d66633e187045730f26788f9af29cc47998d00
PuzZan Karki <mail.puzzan@gmail.com>
2025-06-12 10:10:35 +0000

    feat: store metadata from user request to sessions table

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -34,12 +34,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "cannot create refresh token: %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	arg := db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiresAt,
 	}
