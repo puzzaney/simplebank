1f22744fa996ad5b434efc9439a755a1d98c4ceb
PuzZan Karki <mail.puzzan@gmail.com>
2025-06-24 11:01:03 +0000

    fix: add task processed message to logger

--- worker/task_send_verify_email.go
+++ worker/task_send_verify_email.go
@@ -50,7 +50,7 @@ func (processor *RedisTaskProcessor) ProcessTaskSendVerifyEmail(ctx context.Cont
 	}
 
 	//TODO: send email to user
-	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).Str("email", user.Email).Msg("")
+	log.Info().Str("type", task.Type()).Bytes("payload", task.Payload()).Str("email", user.Email).Msg("processed task")
 
 	return nil
 }
